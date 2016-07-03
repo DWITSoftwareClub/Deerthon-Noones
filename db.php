69a8461d38b0ec929c709e88f78ef9a9c9a5ba6b
firojghimire <firojghimire@gmail.com>
2016-07-03 16:20:02 +0545

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('settings.php');
-$connection=new mysqli($mysql_host,$mysql_username,$mysql_password,$mysql_database);
-if($connection->connect_error) {
-    echo "Failed to connect to MySQL" . $connection->connect_error;
-}
-?>
